c1ecccd499487d4addedd2e264f54a2289c452f2
Carey Li <hello@carey.li>
2018-04-27 14:24:55 +1000

    Rename basic to bojangles

--- flaskr/app.py
+++ flaskr/app.py
@@ -7,8 +7,8 @@ def register_models(app):
     from . import models
 
 def register_blueprints(app):
-    from .basic import app as basic_bp
-    app.register_blueprint(basic_bp)
+    from .bojangles import app as bojangles_bp
+    app.register_blueprint(bojangles_bp)
 
 def create_app():
     app = Flask(__name__)
--- flaskr/bojangles/__init__.py
+++ flaskr/bojangles/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 app = Blueprint(
-    'basic',
+    'bojangles',
     __name__,
     template_folder='templates',
     static_folder='static')
