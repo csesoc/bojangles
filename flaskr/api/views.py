31afffefa794a7b39c0ffad8f18fa0874a13820f
Chen Zhou <anon1mous@users.noreply.github.com>
2018-06-01 20:54:16 +1000

    Changed db format, created courses EP
     - DB now contains course_code: course data entries
     - /courses/<sem>[/<course>] endpoint added
       - /courses/<sem> lists all courses running in a semester as well as
       their descriptions
       - /courses/<sem>/<course> lists data about a specific course

--- flaskr/api/views.py
+++ flaskr/api/views.py
@@ -1,31 +1,26 @@
-from flask import abort, jsonify, current_app
+from flask import jsonify, current_app
 import json
 from . import app
 
-
 """
-Return json for all courses under <spec> specialisation
-in <sem> semester (X1|S1|S2)
+Returns a list of courses running in a specific semester
+Endpoint: /api/courses/(S1 | S2 | X1)
 """
-@app.route('/specialisation/<spec>/<sem>')
-def specialisation(spec, sem):
-    sem = sem.upper()  # accept lower case request
-
+@app.route('/courses/<sem>')
+@app.route('/courses/<sem>/<course>')
+def course(sem, course=None):
     db_file = "%s/%s" % (current_app.root_path, current_app.config['CLASSUTIL_PATH'])
     db = json.load(open(db_file, 'r'))
 
     try:
-        # todo: hesitant: Optimize this if scalability is required
-        #       As it stands there are around 354 entries in the list, which we are searching through the entirety of
-        #       as 354 is small, this is not really an issue, but we could easily optimize it by quitting as soon as
-        #       we find a match, or we could just use a binary search (as the list is sorted by specialisation
-        #       alphabetically)
+        courses = db[sem]
+    except KeyError:
+        return "Semester %s does not exist" % sem, 400
 
-        # We return next as the format guarantees there will only be one specialisation per session
-        return jsonify(next(
-            (x for x in db if x['specialisation'] == spec and x['session'] == sem)
-        ))
-    except StopIteration:
-        # In the case of a empty return from filter, either the specialisation or session is invalid
-        # (as there most be a object even if it has no courses)
-        abort(400)
+    if course is None:
+        return jsonify([(code, data['description']) for code, data in courses.items()])
+    else:
+        try:
+            return jsonify(courses[course])
+        except KeyError:
+            return "Course %s does not exist" % course, 400
